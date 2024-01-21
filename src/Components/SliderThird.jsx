9e23f01ac9d4aa7fbf4f5cd1716d071ba9a40881
TarunThakur7974 <tarunsinghthakur6261@gmail.com>
2024-01-21 16:24:17 +0530

    update third

--- src/Components/SliderThird.jsx
+++ src/Components/SliderThird.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Heading from './SmallComponents/Heading'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import one from '../assets/autoOne.jpg'
@@ -26,6 +26,25 @@ import { Autoplay, Navigation } from 'swiper/modules';
 const SliderThird = () => {
 
     const [arr] = useState([one, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Eleven, Twelve])
+    const [slidesPerView, setSlidesPerView] = useState(3);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 450) {
+                setSlidesPerView(1);
+            }
+            else if (window.innerWidth <= 850) {
+                setSlidesPerView(2);
+            } else {
+                setSlidesPerView(3);
+            }
+        };
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <>
@@ -41,7 +60,7 @@ const SliderThird = () => {
                 <div>
                     <Swiper
                         spaceBetween={8}
-                        slidesPerView={3}
+                        slidesPerView={slidesPerView}
                         loop={true}
                         centeredSlides={true}
                         autoplay={{
@@ -57,7 +76,7 @@ const SliderThird = () => {
                         }
 
                         modules={[Autoplay, Navigation]}
-                        className="mySwiper w-4/6 max-lg:w-3/4 max-md:w-5/6 max-sm:w-[90%]"
+                        className="mySwiper w-4/6 max-lg:w-3/4 max-md:w-5/6"
                     >
                         <div className="swiper-button-next moveBtn">&gt;</div>
                         <div className="swiper-button-prev moveBtn">&lt;</div>
